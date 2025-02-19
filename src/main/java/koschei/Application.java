834d4ea5cd6df198d50436ad9355c1ba2cc95289
Alexandr <ekkib@mail.ru>
2025-02-19 09:49:20 +0300

    решение

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -4,6 +4,8 @@ import koschei.config.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -11,5 +13,7 @@ public class Application {
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
         System.out.println(koscheiTheDeathless.getRulesByDeth());
+
+       Arrays.stream(applicationContext.getBeanDefinitionNames()).distinct().forEach(System.out::println);
     }
 }
